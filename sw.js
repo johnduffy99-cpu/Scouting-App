2192dcf68285360ac8182c27633062d3ed62057d
johnduffy99-cpu <john.duffy99@me.com>
2026-09-26 12:47:10 +0100

    Prepare v1.8.2 P0 player action repair

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='scoutline-v1-8-1-build-2026-09-21-1';
+const CACHE='scoutline-v1-8-2-build-2026-09-26-1';
 const ASSETS=['/','/index.html','/manifest.webmanifest','/icon.svg','/icons/apple-touch-icon.png','/icons/icon-192.png','/icons/icon-512.png','/icons/icon-maskable-512.png','/src/styles.css','/src/main.js','/src/model.js','/src/goalkeeper-workflow.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
